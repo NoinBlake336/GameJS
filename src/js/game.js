0ff7a118616cd1e66687092f81ec482b470d1a58
NoinBlake336 <144173984+NoinBlake336@users.noreply.github.com>
2023-12-02 15:01:44 +0000

    bomb

--- src/js/game.js
+++ src/js/game.js
@@ -20,7 +20,7 @@ const startGame = () =>{
     for (let row = 1; row <=10; row++) {
         for (let cols = 1; cols <= 10; cols++ ){
             game.fillText(emojis[mapRowCols[row - 1][cols - 1]]
-                ,(elementsSize*cols),(elementsSize*row)-20);
+                ,(elementsSize*cols)-20,(elementsSize*row));
         }
     };
 
