e7455401e7d79eea79c69a7d79492afa8581a656
NoinBlake336 <144173984+NoinBlake336@users.noreply.github.com>
2023-12-02 14:02:48 +0000

    commit

--- src/js/game.js
+++ src/js/game.js
@@ -32,7 +32,7 @@ const setcanvasSizes = ()=>{
     canvas.setAttribute('width',canvasSize);
     canvas.setAttribute('height', canvasSize);
 
-    elementsSize = canvasSize / 5;
+    elementsSize = canvasSize / 8;
     console.log(elementsSize);
     startGame();
 };
